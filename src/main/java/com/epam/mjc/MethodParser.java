9b65f46f4d3bd904d02e4a71ee02fcd776f9dc5f
Nozimjon Nabiev <73819566+NozimjonNabiev@users.noreply.github.com>
2023-07-16 21:26:17 +0500

    Done

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
@@ -20,6 +24,79 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String accessModifier = null;
+        String returnType = null;
+        String methodName = null;
+
+        // Create argument list
+        List<MethodSignature.Argument> listOfArguments = new ArrayList<>();
+
+
+        // Divide signatureString into tokens by space
+        StringTokenizer tokenizer =  new StringTokenizer(signatureString, " ");
+        List<String> tokens = new ArrayList<>();
+
+        // Check if tokenizer has token
+        if (tokenizer.hasMoreTokens()) {
+
+            String token = tokenizer.nextToken();
+
+            // Check if a first token is a valid access modifier
+            if (isValidAccessModifier(token))
+                accessModifier = token;
+            else
+                tokenizer = new StringTokenizer(signatureString, " ");
+
+        }
+
+        // Check next token for return type
+        if (tokenizer.hasMoreTokens()) {
+            returnType = tokenizer.nextToken();
+        }
+
+
+        if (tokenizer.hasMoreTokens()) {
+            String methodAndArgs = tokenizer.nextToken();
+            int method = methodAndArgs.indexOf("("); // get the index of the (, as method name ends with open parenthesis
+            methodName = methodAndArgs.substring(0, method); // assign method name
+
+            String firstArg = methodAndArgs.substring(method + 1); // get first argument after open parenthesis
+
+            // Check if the first argument is close parenthesis - if true then it is not an argument
+            if (!firstArg.equals(")"))
+                tokens.add(firstArg);
+
+            // Search for commas and replace them
+            while (tokenizer.hasMoreTokens()) {
+                String token = tokenizer.nextToken().replace(",", "");
+                if (token.contains(")")){
+                    token = token.substring(0, token.length() - 1);
+                }
+                tokens.add(token);
+            }
+        }
+
+
+        if (tokens.size() != 0) {
+            // Assign values to argument type and argument name
+            for (int i = 0; i < tokens.size(); i += 2) {
+                String argType = tokens.get(i);
+                String argName = tokens.get(i + 1);
+
+                listOfArguments.add(new MethodSignature.Argument(argType,argName));
+            }
+        }
+
+        // Create MethodSignature object and assign values
+        MethodSignature methodSignature = new MethodSignature(methodName, listOfArguments);
+        methodSignature.setReturnType(returnType);
+        methodSignature.setAccessModifier(accessModifier);
+        return methodSignature;
+    }
+
+    public boolean  isValidAccessModifier(String accessModifier) {
+        return accessModifier.equals("public") || accessModifier.equals("private")
+                || accessModifier.equals("protected");
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +14,28 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        List<String> result = new ArrayList<>();
+        StringBuilder subStrings = new StringBuilder();
+
+        for (int i = 0; i < source.length(); i++) {
+            String character = String.valueOf(source.charAt(i));
+            if (delimiters.contains(character)) {
+                if (subStrings.length() > 0) {
+                    result.add(subStrings.toString());
+                    subStrings = new StringBuilder();
+                }
+            } else {
+                if (Character.isAlphabetic(source.charAt(i)) || Character.isDigit(source.charAt(i)))
+                    subStrings.append(source.charAt(i));
+            }
+        }
+
+        if (subStrings.length() > 0)
+            result.add(subStrings.toString());
+
+
+
+        return result;
     }
 }
